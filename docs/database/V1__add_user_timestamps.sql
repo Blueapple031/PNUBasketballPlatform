1865db4efb73c8b46ae9c8d121001078ec0125eb
Blueapple031 <61594585+Blueapple031@users.noreply.github.com>
2026-03-04 20:47:55 +0900

    feat: DB설계 최적화

--- docs/database/V1__add_user_timestamps.sql
+++
@@ -1,7 +0,0 @@
--- V1: Add last_login_at and deleted_at to users table
-
-ALTER TABLE users ADD COLUMN last_login_at TIMESTAMP;
-ALTER TABLE users ADD COLUMN deleted_at TIMESTAMP;
-
-COMMENT ON COLUMN users.last_login_at IS '마지막 로그인 시간';
-COMMENT ON COLUMN users.deleted_at IS '소프트 삭제 시간';
